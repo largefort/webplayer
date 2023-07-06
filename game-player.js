45376694774880c2dd2bdab613292d95cd780c32
largefort <106451813+largefort@users.noreply.github.com>
2023-07-06 14:09:13 +0000

    Update game-player.js

--- game-player.js
+++ game-player.js
@@ -2,7 +2,7 @@ window.onload = function() {
     var gameIframe = document.getElementById('game-iframe');
 
     function loadGame(url) {
-        gameIframe.src = url;
+        gameIframe.src = url;https://luts91.github.io/idle-idle-gamedev/
     }
 
     // Example usage: loadGame('https://luts91.github.io/idle-idle-gamedev');
